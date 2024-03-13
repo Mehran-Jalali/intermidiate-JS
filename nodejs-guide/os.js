82ab06811df28368552e6e1cadec8a02722632de
mehran <104734004+Mehran-Jalali@users.noreply.github.com>
2024-03-13 03:42:28 +0330

    nodeJs Operating-System

---
+++ nodejs-guide/os.js
@@ -0,0 +1,30 @@
+// os is referred to operating system
+const os = require("os");
+
+//get type of operating system
+
+// console.log(os.type());
+
+//get platform of operating system
+
+// console.log(os.platform());
+
+//get architcture of operating system
+
+// console.log(os.arch());
+
+//get info about total memory (byte) of operating system
+
+// console.log(os.totalmem());
+
+//get info about free memory (byte) of operating system
+
+// console.log(os.freemem());
+
+//get up time of operating system
+
+// console.log(os.uptime());
+
+//get info about owner of operating system
+
+console.log(os.userInfo());
